d0d096388c852f8fccd7b7d2ed7d7d3942a3312c
Andrei Matei <andrei@cockroachlabs.com>
2023-05-23 14:52:44 -0400

    reading batches works

--- agent.go
+++ agent.go
@@ -5,17 +5,19 @@ import (
 	"encoding/json"
 	"flag"
 	"github.com/go-delve/delve/service/rpc2"
-	"github.com/kr/pretty"
 	"log"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var addrFlag = flag.String("addr", "127.0.0.1:45689", "")
 
 var script = `
 frames_of_interest = {
-  # 'executeWriteBatch': 'ba',
-  'executeRead': 'ba',
+	# 'executeWriteBatch': 'ba',
+	'executeRead': 'ba.Requests[0].Value.(*kvpb.RequestUnion_Get).Get'
+	# 'DistSender': ba,
 }
 
 def gs():
@@ -29,7 +31,7 @@ def gs():
 			False, # full
 			False, # defers
 			7,     # option flags
-			# {"FollowPointers":True, "MaxVariableRecurse":1, "MaxStringLen":0, "MaxArrayValues":0, "MaxStructFields":100}, # MaxVariableRecurse:1, MaxStringLen:64, MaxArrayValues:64, MaxStructFields:-1}"
+			# {"FollowPointers":True, "MaxVariableRecurse":3, "MaxStringLen":0, "MaxArrayValues":10, "MaxStructFields":100}, # MaxVariableRecurse:1, MaxStringLen:64, MaxArrayValues:64, MaxStructFields:-1}"
 			)
 		# Search for frames of interest.
 
@@ -52,14 +54,13 @@ def gs():
 		vars.append(eval(
 				{"GoroutineID": r[0], "Frame": r[1]}, 
 				frames_of_interest[r[2]],
-				{"FollowPointers":True, "MaxVariableRecurse":3, "MaxStringLen":100, "MaxArrayValues":10, "MaxStructFields":100}
-			).Variable)
+				{"FollowPointers":True, "MaxVariableRecurse":5, "MaxStringLen":100, "MaxArrayValues":10, "MaxStructFields":1000}
+			).Variable.Value)
 
 	print("looked at #goroutines: ", len(gs))
 	return json.encode(vars)
 
 def main():
-	print("hello world")
 	return gs()
 `
 
@@ -67,30 +68,39 @@ func main() {
 	flag.Parse()
 	client := rpc2.NewClient(*addrFlag)
 
-	state, err := client.Halt()
-	if err != nil {
-		panic(err)
-	}
-	log.Printf("got state: running:%t", state.Running)
-
-	out, err := client.ExecScript(script)
-	if err != nil {
-		log.Printf("executing script failed: %s\n.Output:%s", err, out.Output)
-	} else {
-		unquoted, err := strconv.Unquote(out.Val)
+	for {
+		_ /* state */, err := client.Halt()
 		if err != nil {
 			panic(err)
 		}
-		var prettyJSON bytes.Buffer
-		if err := json.Indent(&prettyJSON, []byte(unquoted), "", "\t"); err != nil {
-			panic(err)
-		}
-		log.Printf("script output: %sval: (%s)", out.Output, prettyJSON.String())
-	}
+		// log.Printf("got state: running:%t", state.Running)
 
-	ch := client.Continue()
-	for state := range ch {
-		log.Printf("got state: %s", pretty.Sprint(state))
+		out, err := client.ExecScript(script)
+		if err != nil {
+			log.Printf("executing script failed: %s\n.Output:%s", err, out.Output)
+		} else {
+			unquoted, err := strconv.Unquote(out.Val)
+			if err != nil {
+				panic(err)
+			}
+			if strings.Contains(unquoted, "val: ([])\n") {
+				// !!!
+			}
+			var prettyJSON bytes.Buffer
+			if err := json.Indent(&prettyJSON, []byte(unquoted), "", "\t"); err != nil {
+				panic(err)
+			}
+			log.Printf("script output: %sval: (%s)", out.Output, prettyJSON.String())
+		}
+
+		go func() {
+			ch := client.Continue()
+			_ = ch
+			//for state := range ch {
+			//	log.Printf("got state: %s", pretty.Sprint(state))
+			//}
+			//log.Print("finished with continue; channel closed")
+		}()
+		time.Sleep(100 * time.Millisecond)
 	}
-	log.Print("finished with continue; channel closed")
 }
