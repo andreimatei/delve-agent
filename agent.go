4c5b5fc87e3fd409dd791e9970d915a3fd87e60f
Andrei Matei <andreimatei1@gmail.com>
2023-05-25 16:29:37 -0400

    fix stalk walking options

--- agent.go
+++ agent.go
@@ -22,43 +22,70 @@ frames_of_interest = {
 	# 'executeRead': 'ba.Requests[0].Value.(*kvpb.RequestUnion_Get).Get'
 }
 
+def serialize_backtrace(gid):
+	stack = stacktrace(gid,
+		100,   # depth
+		False, # full
+		False, # defers
+		# 7,     # option flags
+		)
+	backtrace = ''
+	for i, f in enumerate(stack.Locations):
+		fun_name = '<unknown>'
+		if f.Location.Function:
+			fun_name = f.Location.Function.Name_
+		backtrace = backtrace + '%d - %s %s:%d (0x%x)\n' % (i, fun_name, f.Location.File, f.Location.Line, f.Location.PC)
+	return backtrace
+
 def gs():
 	gs = goroutines().Goroutines
 
 	res = []
 
+	stacks = {}
 	for g in gs:
 		stack = stacktrace(g.ID,
 			100,   # depth
 			False, # full
 			False, # defers
-			7,     # option flags
+			# 7,     # option flags
 			# {"FollowPointers":True, "MaxVariableRecurse":3, "MaxStringLen":0, "MaxArrayValues":10, "MaxStructFields":100}, # MaxVariableRecurse:1, MaxStringLen:64, MaxArrayValues:64, MaxStructFields:-1}"
 			)
 
 		# Search for frames of interest.
+		backtrace = ''
 		i = 0
 		for f in stack.Locations:
+			fun_name = '<unknown>'
+			if f.Location.Function:
+				fun_name = f.Location.Function.Name_
+			backtrace = backtrace + '%d:%d - %s %s:%d (0x%x)\n' % (g.ID, i, fun_name, f.Location.File, f.Location.Line, f.Location.PC)
 			for foi in frames_of_interest:
 				if not f.Location.Function:
 					continue
-				# print("looking at ", g.ID, i, f.Location.Function.Name_, f.Location.File, f.Location.File.Line)
-				if foi in f.Location.Function.Name_:
-					print("found frame of interest: gid: %d:%d, func: %s, location: %s:%d (0x{%d})" %
+				if f.Location.Function.Name_.endswith(foi):
+					print("found frame of interest: gid: %d:%d, func: %s, location: %s:%d (0x%x)" %
 						(g.ID, i, f.Location.Function.Name_, f.Location.File, f.Location.Line, f.Location.PC))
 					res.append((g.ID, i, foi, f.Location.Function.Name_))
 			i = i+1
-		# print('-----------------------')
+		stacks[g.ID] = backtrace
+
+	if res:
+		print('-----------------------')
+	for r in res:
+		(gid, frame, foi, loc) = r
+		print(stacks[gid])
 
 	print("res: ", res)
 	vars = []
 	for r in res:
 		(gid, frame, foi, loc) = r
 		print("reading from GoroutineID: %d, Frame: %d, foi: %s loc: %s" % (gid, frame, foi, loc)) # , frames_of_interest[r[2]])
+		backtrace = serialize_backtrace(gid)
+		print("backtrace for %d: %s" % (gid, backtrace))
 		vars.append(eval(
-				{"GoroutineID": r[0], "Frame": r[1]}, 
-				frames_of_interest[r[2]],
-				# panicky config: {"FollowPointers":True, "MaxVariableRecurse":2, "MaxStringLen":100, "MaxArrayValues":10, "MaxStructFields":100}
+				{"GoroutineID": gid, "Frame": frame}, 
+				frames_of_interest[foi],
 				{"FollowPointers":True, "MaxVariableRecurse":2, "MaxStringLen":100, "MaxArrayValues":10, "MaxStructFields":100}
 			).Variable.Value)  # .Value
 		print("reading succeed")
@@ -80,6 +107,7 @@ func main() {
 			panic(err)
 		}
 		// log.Printf("got state: running:%t", state.Running)
+		time.Sleep(time.Second)
 
 		out, err := client.ExecScript(script)
 		if err != nil {
@@ -107,6 +135,7 @@ func main() {
 			//}
 			//log.Print("finished with continue; channel closed")
 		}()
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(time.Second)
+		//time.Sleep(100 * time.Millisecond)
 	}
 }
